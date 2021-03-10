b8a2a1dc731ca8db34e3ad0da5858eef90f070a7
baguevaram <38705786+baguevaram@users.noreply.github.com>
2021-03-10 16:13:52 +0000

    Deploying to gh-pages from  @ b9fb0e191af6af9402e50d07c8eeea8ac273240f 🚀

--- docs/sketches/Negative.js
+++ docs/sketches/Negative.js
@@ -7,7 +7,12 @@ function setup() {
 function draw() {
     image(img, 0, 0,width,height);
     loadPixels();
-    pixels = pixels.reverse();
+    for (let i = 0; i < pixels.length; i+=4) {
 
+        pixels[i] = 255-pixels[i] ;
+        pixels[i+1] = 255-pixels[i+1] ;
+        pixels[i+2] = 255-pixels[i+2];
+
+    }
     updatePixels();
 }

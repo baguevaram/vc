3131e27f9caa32ab7d775bfefb443b0d9655c9dd
baguevaram <38705786+baguevaram@users.noreply.github.com>
2021-03-24 23:17:25 +0000

    Deploying to gh-pages from  @ 691efea2e705336f027daab36fb600e9540bd3ff 🚀

--- docs/sketches/tmp.js
+++ docs/sketches/tmp.js
@@ -1,8 +1,10 @@
 let img;
 let grayPixels = [];
+let redPixels = [];
+let greenPixels = [];
+let bluePixels = [];
 let yStep = 8;
-let xStep = 8;
-let slider;
+let xStep = 7;
 
 let W;
 let H;
@@ -18,6 +20,7 @@ function setup() {
     W = img.width;
     H = img.height;
     createCanvas(W, H);
+    textSize(11);
     textAlign(LEFT, TOP);
 
 
@@ -27,32 +30,38 @@ function setup() {
 
     for (let i = 0; i < pixels.length; i += 4) {
         let prom = pixels[i] * 0.21 + pixels[i + 1] * 0.71 + pixels[i + 2] * 0.07;
+        redPixels.push(pixels[i])
+        greenPixels.push(pixels[i + 1])
+        bluePixels.push(pixels[i + 2])
         grayPixels.push(prom)
     }
-
-    slider = createSlider(2, 16, 100);
-    slider.position(10, 10);
-
+    noLoop();
 }
 
 function draw() {
     background(255);
-
-    xStep = slider.value();
-    yStep = xStep;
-
-    fill(0)
-    textSize(xStep + 2);
-
+    fill(0);
     for (let i = 0; i < height - (yStep - 1); i += yStep) {
         for (let j = 0; j < width - (xStep - 1); j += xStep) {
             let gray = 0
+            let red = 0
+            let green = 0
+            let blue = 0
             for (let k = 0; k < yStep; k++) {
                 for (let l = 0; l < xStep; l++) {
                     gray += grayPixels[(W * (i + k)) + (j + l)]
+                    red += redPixels[(W * (i + k)) + (j + l)]
+                    green += greenPixels[(W * (i + k)) + (j + l)]
+                    blue += bluePixels[(W * (i + k)) + (j + l)]
                 }
             }
             gray = Math.round(gray / (yStep * xStep))
+            red = Math.round(red / (yStep * xStep))
+            green = Math.round(green / (yStep * xStep))
+            blue = Math.round(blue / (yStep * xStep))
+
+            fill(red, green, blue)
+
             let index = Math.round(gray * (BASE.length + 1) / 255);
 
             if (index >= BASE.length) {
@@ -63,9 +72,4 @@ function draw() {
 
         }
     }
-
-    textSize(20);
-    rect(10, 30, 28, 20);
-    fill(255);
-    text(xStep, 10, 30);
 }

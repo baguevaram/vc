d3e3bad1bb90fcf7868a3e4e43b4b89eb2ab5f5a
baguevaram <38705786+baguevaram@users.noreply.github.com>
2021-03-29 18:17:27 +0000

    Deploying to gh-pages from  @ c0a309281f39f879ca6c2a73fd9caceeb0dea9d2 🚀

--- docs/sketches/mosaico.js
+++ docs/sketches/mosaico.js
@@ -1,20 +1,90 @@
-let img;
+let img, smaller;
 
 let W;
 let H;
-
+let scl = 8;
+let allImages = [],
+    bright = [],
+    brightImages = new Array(256),
+    imgTemp = [];
 function preload() {
-    img = loadImage('/vc/docs/sketches/test.jpeg');
-
+    img = loadImage("/vc/docs/sketches/obama.jpg");
+    for (var i = 0; i < 7; i++) {
+        imgTemp[i] = loadImage("/vc/docs/sketches/img" + i + ".jpg");
+    }
 }
-
 function setup() {
-    W = img.width;
-    H = img.height;
-    createCanvas(W, H);
+    W = img.width / scl;
+    H = img.height / scl;
+    createCanvas(img.width, img.height);
+    smaller = createImage(W, H);
+    smaller.copy(img, 0, 0, img.width, img.height, 0, 0, W, H);
+    for (var i = 0; i < 7; i++) {
+        allImages[i] = createImage(scl, scl);
+        allImages[i].copy(
+            imgTemp[i],
+            0,
+            0,
+            imgTemp[i].width,
+            imgTemp[i].height,
+            0,
+            0,
+            scl,
+            scl
+        );
+    }
 }
 
 function draw() {
     //TODO
-    image(img, 0, 0, W, H);
+    for (var i = 0; i < 7; i++) {
+        allImages[i].loadPixels();
+        let avg = 0;
+        for (var j = 0; j < allImages[i].pixels.length; j += 4) {
+            let b = brightness(
+                allImages[i].pixels[j],
+                allImages[i].pixels[j + 1],
+                allImages[i].pixels[j + 2]
+            );
+            avg += b;
+        }
+        avg /= allImages[i].pixels.length / 4;
+        bright[i] = avg;
+    }
+    console.log(bright);
+    for (var k = 0; k < brightImages.length; k++) {
+        var record = 256;
+        for (var r = 0; r < bright.length; r++) {
+            var diff = abs(k - bright[r]);
+            if (diff < record) {
+                record = diff;
+                brightImages[k] = allImages[r];
+            }
+        }
+    }
+    smaller.loadPixels();
+    for (let x = 0; x < W; x++) {
+        for (let y = 0; y < H; y++) {
+            let index = 4 * (x + y * W);
+            let c = color(
+                smaller.pixels[index],
+                smaller.pixels[index + 1],
+                smaller.pixels[index + 2]
+            );
+            // fill(brightness(c));
+            // noStroke();
+            // rect(x * scl, y * scl, scl, scl);
+            let imageIndex = brightness(c);
+            // fill(c);
+            // noStroke();
+            // rect(x * scl, y * scl, scl, scl);
+            console.log(imageIndex);
+            if (brightImages[imageIndex] !== undefined)
+                image(brightImages[imageIndex], x * scl, y * scl, scl, scl);
+        }
+    }
+    smaller.updatePixels();
+    //image(img, 0, 0);
+    //   image(smaller, 0, 0);
+    noLoop();
 }

cd0defd42ee447b6b62bacab9dcf5b6431349a52
baguevaram <baguevaram@unal.edu.co>
2021-04-03 11:31:46 -0500

    :twisted_rightwards_arrows: merge cheems

--- docs/sketches/mosaico.js
+++ docs/sketches/mosaico.js
@@ -2,14 +2,16 @@ let img, smaller;
 
 let W;
 let H;
-let scl = 8;
+//obama,test2=8, test=10
+let scl = 10;
 let allImages = [],
     bright = [],
     brightImages = new Array(256),
-    imgTemp = [];
+    imgTemp = [],
+    cantImg = 12;
 function preload() {
-    img = loadImage("/vc/docs/sketches/obama.jpg");
-    for (var i = 0; i < 7; i++) {
+    img = loadImage("/vc/docs/sketches/test.jpeg");
+    for (var i = 0; i < cantImg; i++) {
         imgTemp[i] = loadImage("/vc/docs/sketches/img" + i + ".jpg");
     }
 }
@@ -19,7 +21,7 @@ function setup() {
     createCanvas(img.width, img.height);
     smaller = createImage(W, H);
     smaller.copy(img, 0, 0, img.width, img.height, 0, 0, W, H);
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < cantImg; i++) {
         allImages[i] = createImage(scl, scl);
         allImages[i].copy(
             imgTemp[i],
@@ -37,7 +39,7 @@ function setup() {
 
 function draw() {
     //TODO
-    for (var i = 0; i < 7; i++) {
+    for (var i = 0; i < cantImg; i++) {
         allImages[i].loadPixels();
         let avg = 0;
         for (var j = 0; j < allImages[i].pixels.length; j += 4) {
@@ -51,14 +53,13 @@ function draw() {
         avg /= allImages[i].pixels.length / 4;
         bright[i] = avg;
     }
-    console.log(bright);
     for (var k = 0; k < brightImages.length; k++) {
         var record = 256;
         for (var r = 0; r < bright.length; r++) {
             var diff = abs(k - bright[r]);
             if (diff < record) {
                 record = diff;
-                brightImages[k] = allImages[r];
+                brightImages[int(k / 10)] = allImages[r];
             }
         }
     }
@@ -71,16 +72,12 @@ function draw() {
                 smaller.pixels[index + 1],
                 smaller.pixels[index + 2]
             );
-            // fill(brightness(c));
-            // noStroke();
-            // rect(x * scl, y * scl, scl, scl);
+            //   fill(brightness(c));
+            //   noStroke();
+            //   rect(x * scl, y * scl, scl, scl);
             let imageIndex = brightness(c);
-            // fill(c);
-            // noStroke();
-            // rect(x * scl, y * scl, scl, scl);
-            console.log(imageIndex);
-            if (brightImages[imageIndex] !== undefined)
-                image(brightImages[imageIndex], x * scl, y * scl, scl, scl);
+            if (brightImages[int(imageIndex / 10)] !== undefined)
+                image(brightImages[int(imageIndex / 10)], x * scl, y * scl, scl, scl);
         }
     }
     smaller.updatePixels();

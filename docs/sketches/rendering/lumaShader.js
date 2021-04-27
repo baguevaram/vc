a7ef9a11c40a63259c137c59831edc7e3e1f276e
baguevaram <38705786+baguevaram@users.noreply.github.com>
2021-04-27 22:59:03 +0000

    Deploying to gh-pages from  @ 576e44eb842af6e98a50a9f7261c12124393fff7 🚀

---
+++ docs/sketches/rendering/lumaShader.js
@@ -0,0 +1,30 @@
+let W;
+let H;
+
+let myShader;
+
+function preload() {
+    img = loadImage('/vc/docs/sketches/rendering/texture.jpg');
+    myShader = loadShader("/vc/docs/sketches/rendering/shader.vert", "/vc/docs/sketches/rendering/textureLUMA.frag")
+}
+
+function setup() {
+    W = 500;
+    H = 500;
+    createCanvas(W, H, WEBGL);
+    textureMode(NORMAL);
+    shader(myShader);
+    myShader.setUniform("texture", img)
+
+    noLoop();
+}
+
+function draw() {
+    beginShape();
+    fill(150);
+    vertex(-W / 2, -H / 2, 0, 0, 0);
+    vertex(W / 2, -H / 2, 0, 1, 0);
+    vertex(W / 2, H / 2, 0, 1, 1);
+    vertex(-W / 2, H / 2, 0, 0, 1);
+    endShape();
+}
---
+++ docs/sketches/rendering/shader3.js
@@ -0,0 +1,33 @@
+let W;
+let H;
+
+let myShader;
+
+function preload() {
+    img = loadImage('/vc/docs/sketches/rendering/texture.jpg');
+    myShader = loadShader("/vc/docs/sketches/rendering/shader.vert", "/vc/docs/sketches/rendering/texture3.frag")
+}
+
+function setup() {
+    W = 500;
+    H = 500;
+    createCanvas(W, H, WEBGL);
+    textureMode(NORMAL);
+    shader(myShader);
+    myShader.setUniform("texture", img)
+
+    noLoop();
+}
+
+function draw() {
+    beginShape();
+    fill(150);
+    vertex(-W / 2, -H / 2, 0, 0, 0);
+    fill(255,0,0);
+    vertex(W / 2, -H / 2, 0, 1, 0);
+    fill(0,255,0);
+    vertex(W / 2, H / 2, 0, 1, 1);
+    fill(0,0,255);
+    vertex(-W / 2, H / 2, 0, 0, 1);
+    endShape();
+}

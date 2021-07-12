da391cb4fe90c265b5d016dd97504565c65774d6
jhriverasa <jhriverasa@unal.edu.co>
2021-07-12 09:01:34 +0000

    Deploying to gh-pages from  @ fbfaf5839ff431e3b99dc9b870e70bd1abbf384c 🚀

--- docs/sketches/CurvedLines.js
+++ docs/sketches/CurvedLines.js
@@ -1,12 +1,18 @@
 function setup() {
   createCanvas(600, 600);
+
+  slider = createSlider(1, 5, 1);
+  slider.position(12, 24);
 }
 
 function draw() {
   background(255);
+  let sliderValue = slider.value();
 
-  for (let i = 0; i <= 600; i += 50) {
-    line(0, i, i + 50, 600);
-    line(600, i + 50, i, 0);
+  for (let i = 0; i <= 600; i += (5 - sliderValue + 1) * 10) {
+    line(0, i, i + (5 - sliderValue + 1) * 10, 600);
+    line(600, i + (5 - sliderValue + 1) * 10, i, 0);
   }
+  line(0, 0, 0, 600);
+  line(600, 0, 0, 0);
 }

6b8d29fa51b781d74553720d825e3f2050364c45
baguevaram <38705786+baguevaram@users.noreply.github.com>
2021-07-16 22:59:58 +0000

    Deploying to gh-pages from  @ 9e74115a3ede9cd573292df268c3c3a4bca46bce 🚀

---
+++ docs/sketches/hardware/lumaShaderVideo.js
@@ -0,0 +1,46 @@
+let W;
+let H;
+
+let slider;
+
+let weights = [[0.2989, 0.5870, 0.1140], [0.2120, 0.7010, 0.0870], [0.2126, 0.7152, 0.0722], [0.2627, 0.6780, 0.0593]]
+
+let fingers;
+
+let myShader;
+
+function preload() {
+    fingers = createVideo(['/vc/docs/sketches/fingers.mov', '/vc/docs/sketches/fingers.webm']);
+    fingers.hide(); // by default video shows up in separate dom
+
+    // myShader = loadShader("/vc/docs/sketches/hardware/shader.vert", "/vc/docs/sketches/hardware/textureLUMA.frag")
+}
+
+function setup() {
+    W = 500;
+    H = 500;
+    createCanvas(W, H, WEBGL);
+    textureMode(NORMAL);
+    // shader(myShader);
+
+    slider = createSlider(0, 3, 40);
+    slider.position(10, 10);
+
+    fingers.loop();
+    // noLoop();
+}
+
+function draw() {
+    let posSlider = slider.value();
+
+    // myShader.setUniform("texture", fingers)
+    // myShader.setUniform("weights", weights[posSlider])
+
+    beginShape();
+    fill(150);
+    vertex(-W / 2, -H / 2, 0, 0, 0);
+    vertex(W / 2, -H / 2, 0, 1, 0);
+    vertex(W / 2, H / 2, 0, 1, 1);
+    vertex(-W / 2, H / 2, 0, 0, 1);
+    endShape();
+}
---
+++ docs/sketches/hardware/maskShaderVideo.js
@@ -0,0 +1,54 @@
+let W;
+let H;
+
+let slider;
+
+let masks = [
+    // Identity
+    [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0],
+    // Edges
+    [-1.0, -1.0, -1.0, -1.0, 8.0, -1.0, -1.0, -1.0, -1.0],
+    // Blur
+    [1.0 / 16.0, 2.0 / 16.0, 1 / 16, 2.0 / 16.0, 4.0 / 16.0, 2 / 16, 1.0 / 16.0, 2.0 / 16.0, 1 / 16],
+    // sharpen
+    [0.0, -1.0, 0.0, -1.0, 5.0, -1.0, 0.0, -1.0, 0.0]
+]
+
+let fingers;
+
+let myShader;
+
+function preload() {
+    fingers = createVideo(['/vc/docs/sketches/fingers.mov', '/vc/docs/sketches/fingers.webm']);
+    fingers.hide(); // by default video shows up in separate dom
+    myShader = loadShader("/vc/docs/sketches/hardware/shader.vert", "/vc/docs/sketches/hardware/mask1.frag")
+}
+
+function setup() {
+    W = 500;
+    H = 500;
+    createCanvas(W, H, WEBGL);
+    textureMode(NORMAL);
+    shader(myShader);
+    myShader.setUniform("offset", [1 / img.width, 1 / img.height])
+
+
+    slider = createSlider(0, 3, 40);
+    slider.position(10, 10);
+    fingers.loop();
+}
+
+function draw() {
+    let posSlider = slider.value();
+
+    myShader.setUniform("texture", fingers)
+
+    myShader.setUniform("mask", masks[posSlider])
+
+    beginShape();
+    vertex(-W / 2, -H / 2, 0, 0, 0);
+    vertex(W / 2, -H / 2, 0, 1, 0);
+    vertex(W / 2, H / 2, 0, 1, 1);
+    vertex(-W / 2, H / 2, 0, 0, 1);
+    endShape();
+}

89a2689cf4ad7b95afc558e2ab11c9035328dbaf
baguevaram <baguevaram@unal.edu.co>
2021-07-18 11:36:59 -0500

    :sparkles: zbuffer

--- docs/sketches/rendering/zBufferBefore.js
+++ docs/sketches/rendering/zBufferBefore.js
@@ -3,7 +3,7 @@ let H;
 
 // let myShader;
 
-let x ;
+let x;
 
 function preload() {
     // myShader = loadShader("/vc/docs/sketches/rendering/shader.vert", "/vc/docs/sketches/rendering/zBuffer.frag")
@@ -17,19 +17,22 @@ function setup() {
 }
 
 function draw() {
-    fill(255, 0, 0)
+    strokeWeight(1)
+    fill(255, 0, 0, 0)
     translate(0, 0, 20);
     ellipse(0, 0, 20, 30)
-    fill(0, 255, 0)
-    translate(30, 10,-20);
-    ellipse(0,0,120,80)
-    fill(0, 0, 255)
-    translate(100, 0, -10);
+    fill(0, 255, 0, 0)
+    translate(30, 10, -20);
+    ellipse(0, 0, 120, 80)
+    fill(0, 0, 255, 0)
+    translate(70, 7, -10);
     box(30)
-    fill(0, 255, 255)
-    translate(-180, 0, 300);
+    fill(0, 255, 255, 0)
+    strokeWeight(0.3)
+    translate(-120, 0, 300);
     box(30)
-    fill(255, 0, 255)
-    translate(-300,-100, -3500);
+    strokeWeight(5)
+    fill(255, 0, 255, 0)
+    translate(-300, -100, -3500);
     box(300)
 }
